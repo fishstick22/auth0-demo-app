0b2f51551695a465baa708ecaae4bf9618537c2b
Jeff <jeff.burkholder@gmail.com>
2017-06-27 01:45:45 -0500

    Login, Register, User Profile complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from './services/auth/auth.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  authApp = {
+    title: 'Auth0 Demo App'
+  }
+  
+  constructor(public auth: AuthService) {
+    auth.handleAuthentication();
+  }
 }
