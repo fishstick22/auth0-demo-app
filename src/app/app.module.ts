b7f6ebac2dffe214941009d592deaa8c766f9972
Jeff <jeff.burkholder@gmail.com>
2017-06-26 20:30:18 -0500

    Auth Service complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CallbackComponent } from './components/callback/callback.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
+
+import { AuthService } from './services/auth/auth.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    CallbackComponent,
     HomeComponent,
     ProfileComponent
   ],
@@ -16,7 +21,7 @@ import { ProfileComponent } from './components/profile/profile.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
