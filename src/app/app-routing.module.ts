dea2f6585c4d3e8b60c8f5cf115138bce67ee765
Jeff <jeff.burkholder@gmail.com>
2017-06-27 01:48:17 -0500

    Login, Register, User Profile complete (#3)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
         path:'',
         component: HomeComponent
     },
+    {
+        path:'home',
+        component: HomeComponent
+    },
     { 
         path: 'callback', 
         component: CallbackComponent 
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -13,8 +13,10 @@ export class AuthService {
     responseType: 'token id_token',
     audience: `https://${AUTH_CONFIG.domain}/userinfo`,
     redirectUri: 'http://localhost:4200/callback',
-    scope: 'openid'
+    scope: 'openid profile'
   });
+
+  userProfile:any;
 
   constructor(public router: Router) {}
 
@@ -58,6 +60,21 @@ export class AuthService {
     // access token's expiry time
     const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
+  }
+
+  public getProfile(cb): void {
+    const accessToken = localStorage.getItem('access_token');
+    if (!accessToken) {
+      throw new Error('Access token must exist to fetch profile');
+    }
+
+    const self = this;
+    this.auth0.client.userInfo(accessToken, (err, profile) => {
+      if (profile) {
+        self.userProfile = profile;
+      }
+      cb(err, profile);
+    });
   }
 
 }
